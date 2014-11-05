7dbce936987d90eb1f6d35e3ec2228ac8cfa994f
Sam Reis <sreis@atlassian.com>
2014-11-06 00:15:49 +1100

    parser: add vector to lettuce

--- Parser.hs
+++ Parser.hs
@@ -7,23 +7,26 @@ import Data.Attoparsec.Text
 import Data.Scientific (Scientific)
 import Data.Text hiding (takeWhile)
 import Data.Word
+import qualified Data.List as List
 import qualified Data.Map as Map
 
 
 identifier :: Parser Text
-identifier = takeWhile1 (\c -> not (inClass "({})" c) && isPrint c && not (isSpace c))
+identifier = takeWhile1 (\c -> not (inClass "({[]})" c) && isPrint c && not (isSpace c))
 
 
--- Lettuce is a tree of function literals and applications, with symbols of type i.
+-- Lettuce is a tree of function literals and applications, with vectors and symbols of type i.
 
 data Lettuce i = LFun i (Lettuce i)
                | LApp (Lettuce i) (Lettuce i)
+               | LVec [Lettuce i]
                | LSym i
   deriving Show
 
 lettuce :: Parser (Lettuce Text)
 lettuce = LFun <$> (char '{' *> skipSpace *> identifier) <*> (skipSpace *> lettuce <* skipSpace <* char '}') <|>
           LApp <$> (char '(' *> skipSpace *> lettuce) <*> (skipSpace *> lettuce <* skipSpace <* char ')') <|>
+          LVec <$> (char '[' *> skipSpace *> sepBy lettuce (takeWhile1 isSpace) <* skipSpace <* char ']') <|>
           LSym <$> identifier
 
 data LettuceBinding sym id = LBind {
@@ -41,6 +44,9 @@ lbind = rec Map.empty minBound Map.empty Map.empty
     rec scope id free tab (LApp f e) = LBind id'' free'' tab'' (LApp f' e')
       where (LBind id'  free'  tab'  f') = rec scope id  free  tab  f
             (LBind id'' free'' tab'' e') = rec scope id' free' tab' e
+    rec scope id free tab (LVec es) = List.foldr f (LBind id free tab (LVec [])) es
+      where f e (LBind id free tab (LVec es)) = let (LBind id' free' tab' e') = rec scope id free tab e
+                                                in LBind id' free' tab' (LVec (e' : es))
     rec scope id free tab (LSym sym) = case (Map.lookup sym free, Map.lookup sym scope) of
       (Nothing, Nothing) -> LBind (succ id) (Map.insert sym id free) (Map.insert id sym tab) (LSym id)
       (Just id', _)      -> LBind id free tab (LSym id')
